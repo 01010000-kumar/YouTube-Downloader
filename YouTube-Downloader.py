2fce5a56becede2585038d77a9196e864836050a
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-06-03 15:31:14 +0530

    Add files via upload

--- YouTube-Downloader.py
+++ YouTube-Downloader.py
@@ -1,35 +1,36 @@
 from pytube import *
 
-def playlist(option):
+def playlist(option,location):
 	playlist = Playlist(input("Enter the Url: "))
 	print('Number of videos in playlist: {}'.format(len(playlist.video_urls)))
 	for url in playlist.video_urls:
 		print("Downloading: ",url)
 		if option == 1:
-			YouTube(url).streams.get_highest_resolution().download()
+			YouTube(url).streams.get_highest_resolution().download(location)
 		elif option == 2:
-			YouTube(url).streams.filter(only_audio=True)[-1].download()
+			YouTube(url).streams.filter(only_audio=True)[-1].download(location)
 
 def video(option):
 	url = YouTube(input("Enter the Url: "))
 	if option == 1:
-		url.streams.get_highest_resolution().download()
+		url.streams.get_highest_resolution().download(location)
 	elif option == 2:
-			url.streams.filter(only_audio=True)[-1].download()
+			url.streams.filter(only_audio=True)[-1].download(location)
 
 
 def main():
 	option1 = input("What do you want to download: \n[1] Video \n[2] Playlist\n:")
 	option2 = input("Do you want it to be Audio or Original: \n[1] Original \n[2] Audio\n:")
+	location = input("\nWhere do you want your files too be save: ")
 	if option1 == "1":
 		if option2 == "1":
-			video(1)
+			video(1,location)
 		elif option2 == "2":
-			video(2)
+			video(2,location)
 	elif option1 == "2":
 		if option2 == "1":
-			playlist(1)
+			playlist(1,location)
 		elif option2 == "2":
-			playlist(2)
+			playlist(2,location)
 
 main()
