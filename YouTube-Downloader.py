304de190d369fd407ef021edcad4e5791432034e
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2020-06-24 23:24:12 +0530

    Add files via upload

--- YouTube-Downloader.py
+++ YouTube-Downloader.py
@@ -1,16 +1,40 @@
 from pytube import *
 
 def playlist(option,location):
-	playlist = Playlist(input("Enter the Url: "))
-	print('Number of videos in playlist: {}'.format(len(playlist.video_urls)))
-	for url in playlist.video_urls:
-		print("Downloading: ",url)
-		if option == 1:
-			YouTube(url).streams.get_highest_resolution().download(location)
-		elif option == 2:
-			YouTube(url).streams.filter(only_audio=True)[-1].download(location)
 
-def video(option):
+	url = input("Enter the Url: ")
+
+	playlist = Playlist(url)
+
+	while True:
+		try:
+			playlist = Playlist(url)
+			if (len(playlist.video_urls) is not 0):
+				break
+		except:
+			continue
+
+	print(f'Number of videos in playlist: {len(playlist.video_urls)}')
+
+	if option==1:
+		playlist.download_all()
+
+	elif option==2:
+		for video_url in playlist.video_urls:
+			print("\n	Downloading: ",video_url)
+			i=0
+			while True:
+				i+=1
+				try:
+					YouTube(video_url).streams.filter(audio_only=True)[-1].download(location)
+				except:
+					cont = input("\n	an error occured, try again? (y/n)\n 	")
+					if cont=="y":
+						continue
+					elif cont=="n":
+						break
+
+def video(option,location):
 	url = YouTube(input("Enter the Url: "))
 	if option == 1:
 		url.streams.get_highest_resolution().download(location)
